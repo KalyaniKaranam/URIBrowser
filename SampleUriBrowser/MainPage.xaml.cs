f605d4111be5681543594f9ba4b6468d1f5d55ef
KalyaniKaranam <30898080+KalyaniKaranam@users.noreply.github.com>
2017-09-25 16:17:04 +0530

    Add files via upload

---
+++ SampleUriBrowser/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleUriBrowser
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnurl_Clicked(object sender, EventArgs e)
+        {
+            var url= "https://stackoverflow.com/questions/27605972/open-a-webpage-without-using-webview";
+            Device.OpenUri(new Uri(url));
+        }
+    }
+}
